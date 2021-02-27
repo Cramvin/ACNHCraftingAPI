399178f1cf1164da630da6bf6ca067cac03eb6ad
Cramvin <kevnspielt@gmail.com>
2021-02-27 18:51:08 +0100

    - new start with a better structure

---
+++ ACNHCraftingAPI/src/CraftingAPI.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class CraftingAPI {
+
+	List<Recipe> recipes;
+	List<Material> materials;
+	
+	public CraftingAPI() {
+		
+	}
+	
+}
---
+++ ACNHCraftingAPI/src/Material.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class Material {
+
+	public static List<Material> getMaterials() {
+		return null;
+	}
+	
+}
